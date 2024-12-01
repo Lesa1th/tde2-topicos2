690ec4bf047d0bd5156edc87bd229a0d997521f4
Lesa1th <ana14bmth@gmail.com>
2024-12-01 16:05:44 -0300

    pequena edicao

--- src/main/java/com/example/users/UsersApplication.java
+++ src/main/java/com/example/users/UsersApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UsersApplication {
-
+	// check
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
